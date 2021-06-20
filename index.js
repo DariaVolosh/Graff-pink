cd7724a918cc720f3315df6499ae6ed4d7b677a1
Daria <flowerdefrance@gmail.com>
2021-06-21 01:05:24 +0400

    Navigation for phones was made

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+// 1
 let figures = document.getElementsByTagName("figure");
 
 for (let n = 0; n < 4; n++) {
@@ -17,4 +18,51 @@ for (let n = 0; n < 4; n++) {
 		childNodes[3].style.opacity = 1;
 		childNodes[5].style.opacity = 0;
 	}
+}
+
+
+
+// 2
+let menuButton = document.getElementsByClassName("menu-button")[0];
+let closeButton = document.getElementsByClassName("close-button")[0];
+let navForPhones = document.getElementsByClassName("nav-for-phones")[0];
+let navContent = document.getElementsByClassName("content")[0];
+
+
+
+function closeNav() {
+	navForPhones.animate([{top: "-999px"}],
+	
+	{	
+		duration: 700,
+		fill: "forwards"
+	})
+
+	setTimeout(() => {
+		navForPhones.style.display = "none";
+		navContent.style.borderBottom = "none";
+	}, 700)
+}
+
+menuButton.onclick = () => {
+	navForPhones.style.display = "flex";
+
+	navForPhones.animate([{top: "0px"}],
+	{	
+		duration: 700,
+		fill: "forwards"
+	})
+
+
+	navContent.style.borderBottom = "1px solid #848484";
+}
+
+closeButton.onclick = () => {
+	closeNav();
+}
+
+window.onscroll = () => {
+	if (navForPhones.getBoundingClientRect().y < -130) {
+		closeNav();
+	};
 }
